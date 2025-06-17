5222ab5b826ee45a6cfc728bad78e04e5bda0740
sammy <getembesammy@gmail.com>
2025-06-17 14:30:28 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code in this file!
+// 1. Set the current user
+const currentUser = 'Grace Hopper';
+
+// 2. Create a welcome message using interpolation
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// 3. Shout the welcome message using .toUpperCase()
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// 4. Make a short greeting using the first letter of currentUser
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
